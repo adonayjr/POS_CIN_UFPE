8036dea780d1fa9060f12d78b0fa301b84da3a90
adonayjr <adonayjunior@hotmail.com>
2023-06-02 17:48:47 -0300

    lista 04 incluida

--- LISTA1/1.py
+++ LISTA1/1.py
@@ -1,14 +1,11 @@
-def new_func():
-    # Leitura dos três números float
-    num1 = float(input("Digite o primeiro número: "))
-    num2 = float(input("Digite o segundo número: "))
-    num3 = float(input("Digite o terceiro número: "))
+# Leitura dos três números float
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+num3 = float(input("Digite o terceiro número: "))
 
 # Cálculo da média aritmética
-    media = (num1 + num2 + num3) / 3
+media = (num1 + num2 + num3) / 3
 
 # Impressão dos números e do resultado
-    print("Números digitados:", num1, num2, num3)
-    print("Média aritmética:", media)
-
-new_func()
+print("Números digitados:", num1, num2, num3)
+print("Média aritmética:", media)
