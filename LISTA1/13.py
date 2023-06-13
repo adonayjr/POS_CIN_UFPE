3a6021a861e44b923ceefde3e780085fc4e8318b
adonayjr <adonayjunior@hotmail.com>
2023-06-13 06:05:17 -0300

    python_10

--- LISTA1/13.py
+++ LISTA1/13.py
@@ -2,6 +2,5 @@
 # O Máximo Divisor Comum (MDC) de 2 números inteiros posítivos é o maior
 # número inteiro que divide ambos sem deixar resto. Seu programa deve ler 2
 # números inteiros positivos e determinar o MDC destes 2 números.
-
 # OBS: Não utilizar nenhuma fórmula ou estratégia matemática para calcular o
 # MDEP - coloque o programa para trabalhar para você...
