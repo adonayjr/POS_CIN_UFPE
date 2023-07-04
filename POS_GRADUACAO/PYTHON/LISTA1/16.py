53d4d600f9aa5cd955f77fb84121cebbdf9c49dd
adonayjr <101341916+adonayjr@users.noreply.github.com>
2023-07-04 10:19:45 -0300

    Delete POS_GRADUACAO/PYTHON directory

--- POS_GRADUACAO/PYTHON/LISTA1/16.py
+++
@@ -1,7 +0,0 @@
-# soluções usando os comandos for e while, respectivamente.
-# Ler um número inteiro positivo e dizer se ele é perfeito ou não. 
-# Obs: um número é perfeito se ele é igual à soma de todos os seus divisores (exceto ele mesmo).
-
-
-
-   
